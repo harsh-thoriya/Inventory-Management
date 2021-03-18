5736a2410147fd4a1fcba2518bfe90b97cb570ee
harsh-thoriya <harsh.thoriya@bacancy.com>
2021-03-18 12:43:38 +0530

    mongodb connection file added

---
+++ Node/db-connect.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+require('dotenv').config({path:'../.env'})
+
+mongoose.connect(process.env.MONGODBURL ,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true}).then((resolved)=>{
+    console.log("Database Connected");
+}).catch((rejected)=>{
+    console.log("Database connection unsuccessful");
+});
