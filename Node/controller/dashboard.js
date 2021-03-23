f46c88bb20fde50423cd8c4ef4075ec002d170e0
harsh-thoriya <harsh.thoriya@bacancy.com>
2021-03-23 16:30:52 +0530

    helper functions added for response

--- Node/controller/dashboard.js
+++ Node/controller/dashboard.js
@@ -1,19 +1,25 @@
 const stockModel = require('../models/stock.js')
 const itemModel = require('../models/item.js')
 const ObjectId = require('mongodb').ObjectID;
+const response = require('../utils/responseFormat.js')
 
 const dashboard = async (req,res,next) => {
 
     const id = req.body.id
-
-    const stockData = await stockModel.find({})
+    try{
     
-    const itemData = await itemModel.find({employeeId:new ObjectId(id)})
+        const stockData = await stockModel.find({})
+        
+        const itemData = await itemModel.find({employeeId:new ObjectId(id)})
 
-    res.send({isError:false,result:{
-        stockData,
-        itemData
-    }})
+        response.successResponse(req, res, data = {
+            stockData,
+            itemData
+        })
+    }
+    catch(e){
+        response.errorResponse(req, res, "error occurred while dashboard", code = 500, e)
+    }
 }
 
 module.exports = {dashboard}
